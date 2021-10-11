c4ba391ab61fda46c1f5b0eef62e6b2269322703
Efimov Ilya <bersek123@gmail.com>
2021-10-11 13:20:21 +0300

    full

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -9,19 +9,26 @@ object WordCount {
       .flatMap(line => line.trim().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s"))
       .map(word => (word, 1))
       .reduceByKey(_ + _) // .reduceByKey((x, y) => x + y)
-      .sortBy { case (word, count) => count } // .sortBy(_._2) or .sortBy(pair => pair._2)
-      .map{case (word, count) => s"$word\t$count"}
+      .map { case (word, count) => (count, word) } // поменяли местами
+      .sortBy { case (count, word) => count } // .sortBy(_._2) or .sortBy(pair => pair._2)
+      .map{ case (word, count) => s"$word;$count" }
       .saveAsTextFile(output)
     sc.stop()
   }
 }
 
-
 //.toString.trim().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s")
 
 //sbt package
 
-//spark-submit --class WordCount --master yarn --deploy-mode cluster scala_spark_hadoop_rdd_2.11-0.1.jar
-// /user/hduser/ppkm/ppkm_dataset.csv /user/hduser/ppkm-rdd-out
+//spark-submit --class WordCount --master yarn --deploy-mode cluster scala_spark_hadoop_rdd_2.11-0.1.jar /user/hduser/ppkm/ppkm_dataset.csv /user/hduser/ppkm-rdd-out
 
 //docker cp target/scala-2.11/scala_spark_hadoop_rdd_2.11-0.1.jar gbhdp:/home/hduser/
+
+//wget https://apache-mirror.rbc.ru/pub/apache/spark/spark-2.4.8/spark-2.4.8-bin-hadoop2.7.tgz
+
+//tar xzf spark-2.4.8-bin-hadoop2.7.tgz && rm spark-2.4.8-bin-hadoop2.7.tgz && mv spark-2.4.8-bin-hadoop2.7 spark
+
+//export SPARK_HOME=/home/hduser/spark
+//export PATH=$PATH:$SPARK_HOME/bin
+//export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/
